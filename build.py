951b4ffd0f92c8b24cf855ab0df4d32d64944ba4
Emi <emi@nuka.works>
2026-09-08 21:14:51 +0200

    Bump unified IntelGfx package to 0.3.0

--- build.py
+++ build.py
@@ -111,7 +111,7 @@ def main():
         parser.error('package tool not built; build the Haiku package tool first')
     env = dict(environment)
     env['LD_LIBRARY_PATH'] = str(build / 'objects/linux/lib') + ':' + env.get('LD_LIBRARY_PATH', '')
-    hpkg = out / 'intel_gfx-0.2.0-1-x86_64.hpkg'
+    hpkg = out / 'intel_gfx-0.3.0-1-x86_64.hpkg'
     subprocess.run([str(package), 'create', '-C', str(stage), str(hpkg)],
                    check=True, env=env)
     revision = subprocess.check_output(['git', 'rev-parse', 'HEAD'],
